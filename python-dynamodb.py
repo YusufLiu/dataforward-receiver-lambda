8ce81a8ee43cc90b1979f79472387b95a29d1bd8
Thomas Hoffmann <thomas@senseware.co>
2017-01-05 15:05:20 -0500

    When testing lambda the packet is the request body is the event but when testing through the Api Gateway it is wrapped in an additional request dictionary

--- python-dynamodb.py
+++ python-dynamodb.py
@@ -15,26 +15,31 @@ DYNAMODB_CONTEXT.traps[Rounded] = 0
 
 
 def lambda_handler(event, context):
-    # print("Received event: " + json.dumps(event, indent=2))
     dynamo = boto3.resource('dynamodb').Table('senseware')
 
+    # When testing lambda the packet is the request body is the event
+    # but when testing through the Api Gateway it is wrapped in an additional
+    # request dictionary
+    try:
+        packet = json.loads(event['body'])
+    except KeyError:
+        packet = event
 
-
-    for record in event['data']:
+    for record in packet['data']:
         # DynamoDb wants decimals, not floats
         record['value'] = Decimal(record['value'])
         record['raw'] = Decimal(record['raw'])
 
         # Add the streams meta data to every record (optional)
-        record['sn'] = event['sn']
-        record['site'] = event['site']
-        record['location'] = event['location']
-        record['mod'] = event['mod']
-        record['sid'] = event['sid']
-        record['type'] = event['type']
-        record['cid'] = event['cid']
-        record['name'] = event['name']
-        record['unit'] = event['unit']
+        record['sn'] = packet['sn']
+        record['site'] = packet['site']
+        record['location'] = packet['location']
+        record['mod'] = packet['mod']
+        record['sid'] = packet['sid']
+        record['type'] = packet['type']
+        record['cid'] = packet['cid']
+        record['name'] = packet['name']
+        record['unit'] = packet['unit']
 
         # Give this record an ID
         record['partition'] = str(uuid.uuid4())
@@ -44,3 +49,9 @@ def lambda_handler(event, context):
             TableName='senseware',
             Item=record
         )
+
+    # Return a Success Message
+    return {
+      "statusCode": 200,
+      "body": ""
+    }
